9dbc897448c19f036067bd471d227039cc9f78da
saforer <saforer@gmail.com>
2016-07-11 20:29:06 -0400

    Labels now have words
    Well half the labels.

--- core/src/forer/tann/videogame/puzzles/picross/PicrossLabel.java
+++ core/src/forer/tann/videogame/puzzles/picross/PicrossLabel.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import forer.tann.videogame.utilities.graphics.Colours;
 import forer.tann.videogame.utilities.graphics.Draw;
+import forer.tann.videogame.utilities.graphics.font.TannFont;
 
 /**
  * Created by Forer on 7/11/2016.
@@ -31,13 +32,21 @@ public class PicrossLabel extends Actor {
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
-
-
         batch.setColor(Colours.DARK);
         Draw.fillRectangle(batch, getX(), getY(), getWidth(), getHeight());
 
         batch.setColor(Color.GOLD);
-        Draw.fillRectangle(batch, getX()+BORDER, getY()+BORDER, getWidth()-BORDER*2, getHeight()-BORDER*2);
+        Draw.fillRectangle(batch, getX() + BORDER, getY() + BORDER, getWidth() - BORDER * 2, getHeight() - BORDER * 2);
+
+        batch.setColor(Color.BLACK);
+
+        if(!isX) {
+            TannFont.font.draw(batch, "Words", getX()+(SIZE/4), getY()+(SIZE/4));
+        } else {
+            TannFont.font.draw(batch,"Words",getX()+(SIZE/4), getY()+(SIZE/4));
+        }
+
         super.draw(batch, parentAlpha);
+
     }
 }
