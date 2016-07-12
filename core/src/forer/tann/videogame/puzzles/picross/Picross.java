5c909d6671f87ef88948752ed7cf20d6f4e5ec23
saforer <saforer@gmail.com>
2016-07-11 20:14:03 -0400

    Adding labels!
    Why are the horizontal labels on the bottom wait what am I missing?

--- core/src/forer/tann/videogame/puzzles/picross/Picross.java
+++ core/src/forer/tann/videogame/puzzles/picross/Picross.java
@@ -1,5 +1,6 @@
 package forer.tann.videogame.puzzles.picross;
 
+import forer.tann.videogame.Main;
 import forer.tann.videogame.puzzles.Puzzle;
 
 /**
@@ -8,14 +9,54 @@ import forer.tann.videogame.puzzles.Puzzle;
 public class Picross extends Puzzle {
     int width = 5;
     int height = 5;
+    int tileSize = PicrossTile.getSize();
     PicrossTile[][] tiles = new PicrossTile[width][height];
     public Picross() {
+        //Draw Labels, Don't draw the 0,0 label, it's blank
+        //Draw the horizontal labels
+        for (int x=0; x<width;x++) {
+            PicrossLabel label = new PicrossLabel(x,-1);
+            addActor(label);
+
+            //Gotta put the picross in the right place
+            int x1 = (Main.width/2);
+            int y1 = (Main.height/2);
+            //Move the picross down and left because we're looking at the bottom left of the picross
+            x1 -= tileSize * width * .5f;
+            y1 -= tileSize * height * .5f;
+            setPosition(x1, y1);
+        }
+        //Draw the vertical labels
+        for (int y=0; y<height;y++) {
+            PicrossLabel label = new PicrossLabel(-1,y);
+            addActor(label);
+
+            //Gotta put the picross in the right place
+            int x1 = (Main.width/2);
+            int y1 = (Main.height/2);
+            //Move the picross down and left because we're looking at the bottom left of the picross
+            x1 -= tileSize * width * .5f;
+            y1 -= tileSize * height * .5f;
+            setPosition(x1, y1);
+        }
+
+        //Draw Tiles
         for(int x=0;x<width;x++){
             for(int y=0;y<height;y++){
                 PicrossTile tile = new PicrossTile(x,y);
                 tiles[x][y] = tile;
                 addActor(tile);
+
+
+                //Gotta put the picross in the right place
+                int x1 = (Main.width/2);
+                int y1 = (Main.height/2);
+                //Move the picross down and left because we're looking at the bottom left of the picross
+                x1 -= tileSize * x * .5f;
+                y1 -= tileSize * y * .5f;
+                setPosition(x1, y1);
             }
         }
+
     }
 }
--- core/src/forer/tann/videogame/puzzles/picross/PicrossLabel.java
+++ core/src/forer/tann/videogame/puzzles/picross/PicrossLabel.java
@@ -3,32 +3,21 @@ package forer.tann.videogame.puzzles.picross;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import forer.tann.videogame.utilities.graphics.Colours;
 import forer.tann.videogame.utilities.graphics.Draw;
 
 /**
  * Created by Forer on 7/11/2016.
  */
-public class PicrossTile extends Actor {
+public class PicrossLabel extends Actor {
     static final int BORDER = 1;
     static final int SIZE = 10;
     int gridX, gridY;
-    boolean selected = false;
-    public PicrossTile(int x, int y) {
+    public PicrossLabel(int x, int y) {
         this.gridX=x;
         this.gridY=y;
         setSize(SIZE, SIZE);
-        setPosition(x*SIZE, y*SIZE);
-        addListener(new InputListener(){
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                System.out.println("you tapped on "+gridX+":"+gridY);
-                selected = !selected;
-                return super.touchDown(event, x, y, pointer, button);
-            }
-        });
+        setPosition(x * SIZE, y * SIZE);
     }
 
     @Override
@@ -38,15 +27,8 @@ public class PicrossTile extends Actor {
         batch.setColor(Colours.DARK);
         Draw.fillRectangle(batch, getX(), getY(), getWidth(), getHeight());
 
-        Color selectionColor = Colours.LIGHT;
-        if (selected) selectionColor = Colours.random();
-        batch.setColor(selectionColor);
+        batch.setColor(Color.GOLD);
         Draw.fillRectangle(batch, getX()+BORDER, getY()+BORDER, getWidth()-BORDER*2, getHeight()-BORDER*2);
         super.draw(batch, parentAlpha);
     }
-
-    static public int getSize() {
-        return SIZE;
-    }
-
 }
